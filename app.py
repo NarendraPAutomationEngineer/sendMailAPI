b0bbaf564ab79435a72f9e669cf0ab8657ac3d8f
NarendraP <60044869+NarendraPAutomationEngineer@users.noreply.github.com>
2022-05-09 06:59:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,6 @@ app.config['MAIL_PASSWORD'] = gPass
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
-@app.before_request
-def only_json():
-    if not request.is_json: 
-        abort(400)
-
-@app.errorhandler(400)
-def not_data(error):
-    response=jsonify({"Error": error.description })
-    response.status_code=400
-    return response
 
 @app.route("/mail", methods=['POST'])
 def sendMailWithCustomBody():
@@ -43,22 +33,5 @@ def sendMailWithCustomBody():
     response.status_code = 201
     return response
 
-@app.route("/secrets", methods=['GET'])
-def secrets():
-    data={
-        "secrets": {
-            "remoteServerCreds": {
-                "user": "ubuntu",
-                "pass" : "ubuntu@123"
-            },
-            "awsAccessKeys" : {
-                "aws_access_key_id": "LIVEARET45LJASDF9EWR",
-                "aws_secret_access_key": "4GAbEcDDDeyYRNO4WBrnYeWgVa-YTDrV6GIJMqQabc"
-            }       
-        }
-    }
-    response = jsonify(data)
-    response.status_code = 200
-    return response
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
